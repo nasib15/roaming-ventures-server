2a634189fd83ff6229cebe5d79fe3d5e1e059c30
knh-nehal <nasibhossain57@gmail.com>
2024-04-25 22:43:09 +0600

    basic project setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
