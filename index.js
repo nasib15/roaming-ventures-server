8ddbd9ede5bd60493430ed8b20ce820517d867fe
Knh Nehal <142614275+knh-nehal@users.noreply.github.com>
2024-05-31 16:27:56 +0600

    update corsoption for server fix

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://b9-a10-client-side-knh-nehal.vercel.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
 // Middleware
 
 app.use(cors());
